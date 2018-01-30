f3733a27e8cd647fc4da7b1226a804178544a133
Richard Wall <richard.wall@jetstack.io>
2018-01-30 10:00:00 +0000

    a script to pull docker images in a dind cluster

---
+++ hack/dind-cluster-pull-images.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# dind-cluster can't pull from private registries
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function docker_login_from_node() {
+    local node=$1
+    local token=$2
+    docker exec "${node}" docker login --username oauth2accesstoken --password "${token}" https://gcr.io
+}
+
+function docker_pull_images_from_node() {
+    local node=$1
+    for component in apiserver controller pilot-elasticsearch pilot-cassandra;
+    do
+        docker exec "${node}" docker pull "gcr.io/jetstack-sandbox/navigator-${component}:build"
+    done
+}
+
+function main() {
+    local nodes=$(kubectl get nodes -o 'jsonpath={.items[*].metadata.name}')
+    local token=$(gcloud auth application-default print-access-token)
+    for n in $nodes
+    do
+        docker_login_from_node "${n}" "${token}"
+        docker_pull_images_from_node "${n}"
+    done
+}
+
+main
