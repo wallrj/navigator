6e4d4286d2b8def7a28b59e126db10129c557084
Richard Wall <richard.wall@jetstack.io>
2018-03-09 11:15:33 +0000

    a headless service for all C* nodes in all node pools

---
+++ pkg/controllers/cassandra/service/nodes/control.go
@@ -0,0 +1,84 @@
+package nodes
+
+import (
+	"fmt"
+
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/golang/glog"
+	"github.com/jetstack/navigator/pkg/controllers/cassandra/util"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/kubernetes"
+	corelisters "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/record"
+
+	v1alpha1 "github.com/jetstack/navigator/pkg/apis/navigator/v1alpha1"
+)
+
+type Interface interface {
+	Sync(*v1alpha1.CassandraCluster) error
+}
+
+type control struct {
+	kubeClient    kubernetes.Interface
+	serviceLister corelisters.ServiceLister
+	recorder      record.EventRecorder
+}
+
+var _ Interface = &control{}
+
+func NewControl(
+	kubeClient kubernetes.Interface,
+	serviceLister corelisters.ServiceLister,
+	recorder record.EventRecorder,
+) Interface {
+	return &control{
+		kubeClient:    kubeClient,
+		serviceLister: serviceLister,
+		recorder:      recorder,
+	}
+}
+
+func (c *control) Sync(cluster *v1alpha1.CassandraCluster) error {
+	service := ServiceForCluster(cluster)
+	_, err := c.serviceLister.Services(service.Namespace).Get(service.Name)
+	if err == nil {
+		glog.V(4).Infof("Service already exists: %s", service.Name)
+		return nil
+	}
+	if !k8sErrors.IsNotFound(err) {
+		return err
+	}
+	glog.V(4).Infof("Creating service: %s", service.Name)
+	_, err = c.kubeClient.CoreV1().Services(service.Namespace).Create(service)
+	if k8sErrors.IsAlreadyExists(err) {
+		glog.V(4).Infof("Service exists: %s", service.Name)
+		return nil
+	}
+	return err
+}
+
+func ServiceForCluster(
+	cluster *v1alpha1.CassandraCluster,
+) *apiv1.Service {
+	clusterLabels := util.ClusterLabels(cluster)
+	return &apiv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            fmt.Sprintf("%s-nodes", util.ResourceBaseName(cluster)),
+			Namespace:       cluster.Namespace,
+			OwnerReferences: []metav1.OwnerReference{util.NewControllerRef(cluster)},
+			Labels:          clusterLabels,
+		},
+		Spec: apiv1.ServiceSpec{
+			ClusterIP: "None",
+			Type:      apiv1.ServiceTypeClusterIP,
+			Selector:  clusterLabels,
+			// Headless service should not require a port.
+			// But without it, DNS records are not registered.
+			// See https://github.com/kubernetes/kubernetes/issues/55158
+			Ports: []apiv1.ServicePort{{Port: 65535}},
+		},
+	}
+}
---
+++ pkg/controllers/cassandra/service/nodes/control_test.go
@@ -0,0 +1,96 @@
+package nodes_test
+
+import (
+	"testing"
+
+	casstesting "github.com/jetstack/navigator/pkg/controllers/cassandra/testing"
+
+	"github.com/jetstack/navigator/internal/test/unit/framework"
+	v1alpha1 "github.com/jetstack/navigator/pkg/apis/navigator/v1alpha1"
+	"github.com/jetstack/navigator/pkg/controllers"
+	"github.com/jetstack/navigator/pkg/controllers/cassandra/service/nodes"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func TestSync(t *testing.T) {
+	cluster1 := casstesting.ClusterForTest()
+
+	type testT struct {
+		kubeObjects        []runtime.Object
+		navObjects         []runtime.Object
+		cluster            *v1alpha1.CassandraCluster
+		fixtureManipulator func(*testing.T, *framework.StateFixture)
+		assertions         func(*testing.T, *controllers.State, testT)
+		expectErr          bool
+	}
+
+	tests := map[string]testT{
+		"create service": {
+			cluster: cluster1,
+			assertions: func(t *testing.T, state *controllers.State, test testT) {
+				expectedService := nodes.ServiceForCluster(cluster1)
+				_, err := state.Clientset.
+					CoreV1().
+					Services(expectedService.Namespace).
+					Get(expectedService.Name, v1.GetOptions{})
+				if err != nil {
+					t.Error(err)
+				}
+			},
+		},
+
+		"service exists": {
+			kubeObjects: []runtime.Object{nodes.ServiceForCluster(cluster1)},
+			cluster:     cluster1,
+		},
+		"not yet listed": {
+			kubeObjects: []runtime.Object{},
+			cluster:     cluster1,
+			fixtureManipulator: func(t *testing.T, fixture *framework.StateFixture) {
+				service := nodes.ServiceForCluster(cluster1)
+				_, err := fixture.KubeClient().CoreV1().Services(service.Namespace).Create(service)
+				if err != nil {
+					t.Fatal(err)
+				}
+			},
+		},
+	}
+
+	for title, test := range tests {
+		t.Run(
+			title,
+			func(t *testing.T) {
+				fixture := &framework.StateFixture{
+					T:                t,
+					KubeObjects:      test.kubeObjects,
+					NavigatorObjects: test.navObjects,
+				}
+				fixture.Start()
+				defer fixture.Stop()
+				if test.fixtureManipulator != nil {
+					test.fixtureManipulator(t, fixture)
+				}
+				state := fixture.State()
+				c := nodes.NewControl(
+					state.Clientset,
+					state.ServiceLister,
+					state.Recorder,
+				)
+				err := c.Sync(test.cluster)
+				if err == nil {
+					if test.expectErr {
+						t.Error("Expected an error")
+					}
+				} else {
+					if !test.expectErr {
+						t.Error(err)
+					}
+				}
+				if test.assertions != nil {
+					test.assertions(t, state, test)
+				}
+			},
+		)
+	}
+}
